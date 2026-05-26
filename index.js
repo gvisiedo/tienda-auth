6dc4dcb8ab3277b3ce870b23c496b2ef7f44a46d
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2026-05-26 08:09:37 +0200

    working on index

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/auth')
+.then(()=>console.log('Conectado a MongoDB'))
+.catch(error => console.log('Error de conexión:', error))
+
+//Rutas
+const authRoutes = require('./routes/auth')
+app.use('/auth', authRoutes)
+
+const productosRoutes = require('./routes/productos')
+app.use('/productos', productosRoutes)
+
+app.listen(3000, function(){
+    console.log('Servidor corriendo en http://localhost:3000')
+})
